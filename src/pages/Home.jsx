06dfc5e9f62e7d24bf0276b75bde12fa56609d31
IvanT1707 <142747086+IvanT1707@users.noreply.github.com>
2025-05-15 19:08:53 +0300

    фікс проблем з деплоєм

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ function Home() {
         <section className="hero-section" aria-labelledby="hero-title">
           <h1 id="hero-title" className="hero">Оренда спортивного обладнання легко та швидко!</h1>
           <h2 className="desc">Обирайте найкраще спорядження для будь-яких втіх</h2>
-          <a href="/rent">
+          <a href="./rent">
             <button className="hero-button" aria-label="Переглянути доступне спортивне обладнання">
               Орендуй зараз!
             </button>
