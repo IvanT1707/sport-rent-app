5635d7a727edc93a1d2a54b0d43ae7093d600557
IvanT1707 <142747086+IvanT1707@users.noreply.github.com>
2025-05-16 15:36:02 +0300

    fff

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
+const express = require('express');
+const path = require('path');
 
 const app = express();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
